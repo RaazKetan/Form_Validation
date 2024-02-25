11af0e3914d41bd2a6928a5a4dfc9800d6eb7477
Raaz <21ketanraaz@gmail.com>
2024-02-25 12:29:45 +0530

    Managaing the ovrall form validity

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
@@ -7,6 +7,10 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+   if(enteredNameIsValid){
+     formIsValid(true);
+   }
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
@@ -44,7 +48,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button >Submit</button>
       </div>
     </form>
   );
